9bfab89b0cbe530b404099d4e968c4d1caf2bc14
Bari Shavershian <108943301+BariShavershian@users.noreply.github.com>
2022-12-25 16:51:34 +0200

    Add files via upload

---
+++ js/jquery.main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+        $(function() {
+            $('.header__logo__nav-btn').click(function(){
+        
+                $('.header__logo__nav').toggleClass('active');
+              
+
+            })
+        });
+    });
